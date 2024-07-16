4cefe91f03843b2551e6a1000bda6ac8d6e99c2f
Christine Dang <dangc101@gmail.com>
2024-07-16 14:43:51 -0400

    updated illustrations page layout and homepage About Me info

--- homepage.js
+++ homepage.js
@@ -19,11 +19,41 @@
 //     }
 
 
-  $(window).on('scroll', function() {
-    var header = $(".header");
-    if ($(this).scrollTop() > 50) {
-        if (!header.data('faded')) header.data('faded', 1).stop(true).fadeTo(400, 0.5);
-    } else if (header.data('faded')) {
-        header.data('faded', 0).stop(true).fadeTo(400, 1);
+//   (window).on('scroll', function() {
+//     var header = $(".header");
+//     if ($(this).scrollTop() > 50) {
+//         if (!header.data('faded')) header.data('faded', 1).stop(true).fadeTo(400, 0.5);
+//     } else if (header.data('faded')) {
+//         header.data('faded', 0).stop(true).fadeTo(400, 1);
+//     }
+// });
+
+
+
+
+
+window.onload = function(e){ 
+    document.getElementById("defaultOpen").click();
+ }  
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-});
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+
+
+
+
+
+
+
